b6559aed34964b3a43c2baf3aafcd7215a596f70
consuman <n.mauersberg@gmail.com>
2020-11-18 16:46:16 +0100

    Basic Component structure with scoped css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Person from './Person/Person'
 const App = () => {
   return (
     <div className="App">
-      <p>Some React Components:</p>
-      <Person name="Jane"/>
-      <Person name="Dorian"/>
+      <h1>Some React Components:</h1>
+      <Person name="Jane" age="26">Hi, I am the Person's Child.</Person>
+      <Person name="Dorian" age="36"/>
     </div>
   );
 }
