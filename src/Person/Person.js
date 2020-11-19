a0e80f05bb54f9aaf44965c85a36b791849ee9ab
nmauersberg <n.mauersberg@gmail.com>
2020-11-19 14:58:49 +0100

    Testing signed commits

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -17,8 +17,14 @@ const Person = (props) => {
             <p>My name is {personName} and I am {props.age} years old!</p>
             <p>{followers} people are following me.</p>
             <div className="buttonRow">
-                <button onClick={() => setFollower(followers + 1)}>Add new follower</button>
-                <input className="inputField" type="text" placeholder="Change name" onChange={(evt) => setNewName(evt.target.value)} />
+                <button onClick={() => setFollower(followers + 1)}>
+                    Add new follower
+                </button>
+                <input
+                    className="inputField"
+                    type="text"
+                    placeholder="Change name"
+                    onChange={(evt) => setNewName(evt.target.value)} />
             </div>
             { hasChildren ?
                 childElement : null
